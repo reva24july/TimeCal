bf7509ac35c1754f68092250e33933d0a0eed4b3
reva24july <reva24july@gmail.com>
2023-06-18 14:53:50 -0500

    Initial commit

---
+++ src/CalculateTime.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+public class CalculateTime {
+
+    public static void main(String[] args) {
+
+      System.out.println(LocalTime.now());
+        System.out.println("Hello world!");
+    }
+}
+
